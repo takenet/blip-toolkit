5153c24968abb894e8f87bc9614a9ca203443f6a
Samuel Martins da Silva <samuels@take.net>
2018-05-23 16:55:55 -0300

    Add prettier file and eslint integration

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,19 +1,17 @@
-"use strict";
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const baseWebpackConfig = require("./webpack.base.conf");
+'use strict'
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const baseWebpackConfig = require('./webpack.base.conf')
 
 module.exports = merge(baseWebpackConfig, {
-  mode: "development",
+  mode: 'development',
   bail: true,
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      pkg: require("../package.json"),
-      template: "./src/index.html",
-      inject: "body"
-    })
-  ]
-});
+      filename: 'index.html',
+      pkg: require('../package.json'),
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
+})

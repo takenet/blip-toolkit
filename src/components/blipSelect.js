57537f1390fe02ee1338de575ff5d8b8ba9be324
Samuel Martins da Silva <samuels@take.net>
2018-05-24 18:53:44 -0300

    Add some functionalities and event handlers

--- src/components/blipSelect.js
+++ src/components/blipSelect.js
@@ -1,11 +1,15 @@
 import { strToEl, guid } from '../lib/utils'
+import { EventEmitter } from '../lib/eventEmitter'
+
+const ANIMATION_TIMEOUT = 300
 
 const bpInputWrapperClass = 'bp-input-wrapper'
 const blipSelectClass = 'blip-select'
 const bpInputWrapperLabelClass = 'bp-label'
-const blipSelectTriggerClass = 'blip-select__trigger'
+const blipSelectInputClass = 'blip-select__input'
 const blipSelectOptionsClass = 'blip-select__options'
 const blipSelectOptionClass = 'blip-select__option'
+const blipSelectOptionSeletedClass = 'blip-select__option--selected'
 
 export class BlipSelect {
   constructor(element, options) {
@@ -13,6 +17,11 @@ export class BlipSelect {
     this.elementLabel = ''
     this.selectOptions = []
     this.selectOptionsContainer = ''
+    this.selectLabel = ''
+    this.isSelectOpen = false
+    this._handleSelectFocus = ''
+    this._handleSelectBlur = ''
+    this._handleOptionClick = ''
 
     this.configOptions = {
       label: '',
@@ -20,10 +29,12 @@ export class BlipSelect {
       afterOpenSelect: () => {},
       beforeCloseSelect: () => {},
       afterCloseSelect: () => {},
+      onSelectOption: ($event) => {}, // { value: optionValue, label: optionLabel }
       ...options,
     }
     this.el = element
     this._setup()
+    this._setupEventHandlers()
   }
 
   /**
@@ -36,7 +47,7 @@ export class BlipSelect {
     this.wrapper = strToEl(`
       <div class="${bpInputWrapperClass} ${blipSelectClass}">
         <label class="${bpInputWrapperLabelClass}">${this.configOptions.label}</label>
-        <input class="${blipSelectTriggerClass}" data-target="${this.customSelectId}" readonly>
+        <input class="${blipSelectInputClass} bp-c-cloud" data-target="${this.customSelectId}" readonly>
         <ul class="${blipSelectOptionsClass}" id="${this.customSelectId}"></ul>
       </div>
     `)
@@ -46,25 +57,86 @@ export class BlipSelect {
     const elementOptions = this.el.querySelectorAll('option')
     this.selectOptionsContainer = this.wrapper.querySelector(`#${this.customSelectId}`)
 
+    this.selectLabel = this.wrapper.querySelector('label')
+
     // Setup element options
     elementOptions.forEach(({ value, label }) => {
       this.selectOptions = this.selectOptions.concat({ value, label })
     })
 
     // Add options to container
-    this.selectOptions.forEach(({ label }) => {
-      this.selectOptionsContainer.appendChild(strToEl(`<li class="${blipSelectOptionClass}">${label}</li>`))
+    this.selectOptions.forEach(({ value, label }) => {
+      this.selectOptionsContainer.appendChild(
+        strToEl(`
+          <li class="${blipSelectOptionClass}" data-value="${value}">${label}</li>
+        `)
+      )
     })
 
     // Setup element trigger
-    this.selectTrigger = this.wrapper.querySelector(`input[data-target="${this.customSelectId}"]`)
-    this.selectTrigger.addEventListener('focus', this._onSelectFocus.bind(this))
-    this.selectTrigger.addEventListener('blur', this._onSelectBlur.bind(this))
+    this.input = this.wrapper.querySelector(`input[data-target="${this.customSelectId}"]`)
 
     // Remove default select display
     this.el.style.display = 'none'
   }
 
+  /**
+   * Setup select event handlers
+   */
+  _setupEventHandlers() {
+    // Assign binded methods to class properties help remove this event handlers in the future
+    this._handleSelectFocus = this._onSelectFocus.bind(this)
+    this._handleSelectBlur = this._onSelectBlur.bind(this)
+    this._handleCenterOption = this._centerSelectedOption.bind(this)
+
+    this.input.addEventListener('focus', this._handleSelectFocus)
+    this.input.addEventListener('blur', this._handleSelectBlur)
+
+    // Set handler for each menu option
+    this.selectOptionsContainer
+      .querySelectorAll('li')
+      .forEach(o => o.addEventListener('click', (ev) => this._onOptionClick(ev)))
+
+    this.selectOptionsContainer.addEventListener('transitionend', this._handleCenterOption)
+  }
+
+  /**
+   * Set value to input
+   * @param {Object} param0 - value/label pair
+   */
+  _setInputValue({ value, label }) {
+    this.input.value = label
+    this.configOptions.onSelectOption(EventEmitter({ value, label }))
+  }
+
+  /**
+   * On select option item
+   * @param {DOMEvent} event
+   */
+  _onOptionClick(event) {
+    if (this.isSelectOpen) {
+      this._setInputValue(this.selectOptions.find(o => o.value === event.target.getAttribute('data-value')))
+      this._resetSelectedOptions()
+      event.target.classList.add(blipSelectOptionSeletedClass)
+    }
+  }
+
+  /**
+   * Remove all selected class from options
+   */
+  _resetSelectedOptions() {
+    this.selectOptionsContainer
+      .querySelectorAll('li')
+      .forEach(o =>
+        o.classList.contains(blipSelectOptionSeletedClass)
+          ? o.classList.remove(blipSelectOptionSeletedClass)
+          : ''
+      )
+  }
+
+  /**
+   * On select click
+   */
   _onSelectFocus() {
     // Callback invoked before select open
     this.configOptions.beforeOpenSelect()
@@ -75,11 +147,16 @@ export class BlipSelect {
     this.configOptions.afterOpenSelect()
   }
 
+  /**
+   * On select blur
+   */
   _onSelectBlur() {
     // Callback invoked before select open
     this.configOptions.beforeCloseSelect()
 
-    this._closeSelect()
+    setTimeout(() => { // Needed for get option value on "li" click
+      this._closeSelect()
+    }, ANIMATION_TIMEOUT - 200)
 
     // Callback invoked after select open
     this.configOptions.afterCloseSelect()
@@ -96,7 +173,28 @@ export class BlipSelect {
       this.selectOptionsContainer.style.opacity = 1
     })
 
-    this.selectTrigger.parentNode.classList.add('bp-input-wrapper--focus')
+    this.input.parentNode.classList.add('bp-input-wrapper--focus')
+    this.selectLabel.classList.add('bp-c-blip-light')
+    this.isSelectOpen = true
+  }
+
+  /**
+   * If has a selected option, center scroll to element
+   */
+  _centerSelectedOption(ev) {
+    const selectedOption = this.selectOptionsContainer.querySelector(`li.${blipSelectOptionSeletedClass}`)
+    if (this.selectOptionsContainer.scrollHeight > this.selectOptionsContainer.clientHeight && ev.propertyName === 'transform') {
+      if (!selectedOption) {
+        return
+      }
+
+      let scrollOffset =
+            selectedOption.getBoundingClientRect().top -
+            this.selectOptionsContainer.getBoundingClientRect().top // scroll to selected option
+      scrollOffset -= this.selectOptionsContainer.clientHeight / 2 // center in dropdown
+
+      this.selectOptionsContainer.scrollTop = scrollOffset
+    }
   }
 
   /**
@@ -108,8 +206,10 @@ export class BlipSelect {
 
     setTimeout(() => { // Needed for animation
       this.selectOptionsContainer.style.display = 'none'
-    }, 300)
+    }, ANIMATION_TIMEOUT) // Milliseconds should be greater than value setted on transition css property
 
-    this.selectTrigger.parentNode.classList.remove('bp-input-wrapper--focus')
+    this.input.parentNode.classList.remove('bp-input-wrapper--focus')
+    this.selectLabel.classList.remove('bp-c-blip-light')
+    this.isSelectOpen = false
   }
 }

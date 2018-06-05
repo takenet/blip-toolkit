7a6d8812229f7e3eb225489f4ae8812aa74c17a6
samx <samuelmartins.sw@gmail.com>
2018-06-05 15:42:39 -0300

    Add event to jasmine env

--- spec/helpers/jasmine_env.js
+++ spec/helpers/jasmine_env.js
@@ -7,3 +7,4 @@ const jsdomInstance = new JSDOM('<html><head></head><body></body></html>')
 global.document = jsdomInstance.window.document
 global.window = jsdomInstance.window
 global.Element = jsdomInstance.window.Element
+global.Event = jsdomInstance.window.Event

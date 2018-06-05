e375fa03826aa552d7be748f05ed5acd054347c0
samx <samuelmartins.sw@gmail.com>
2018-06-05 15:43:39 -0300

    Removed sandbox instance tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,2 @@
 import './scss/main.scss'
-import { BlipSelect } from './components/blipSelect/blipSelect'
-
-const instance = new BlipSelect(document.getElementById('my-select'), {
-  label: 'Custom select',
-  placeholder: 'My placeholder',
-  onSelectOption: ({ $event: { value, label } }) => console.log(value, label),
-})
-
-const { value, label } = instance.getValue()
-window.test = () => console.log(value, label) //eslint-disable-line
+export { BlipSelect } from './components/blipSelect'

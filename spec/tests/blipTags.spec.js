06b0a49953b02690872dc9b032c2c3437a7a70ae
samx <samuelmartins.sw@gmail.com>
2018-06-21 15:49:22 -0300

    Add BlipTags tests

---
+++ spec/tests/blipTags.spec.js
@@ -0,0 +1,50 @@
+import { BlipTags } from '../../src/components/blipTags'
+
+describe('BlipTags', () => {
+  let tagList
+
+  beforeEach(() => {
+    tagList = document.createElement('div')
+    tagList.id = 'my-tag-list'
+    document.body.appendChild(tagList)
+  })
+
+  describe('Instance', () => {
+    it('should return a BlipTags instance', () => {
+      const tags = new BlipTags(tagList)
+
+      expect(tags instanceof BlipTags).toBeTruthy()
+    })
+  })
+
+  describe('Methods', () => {
+    describe('Handle tags', () => {
+      describe('Add', () => {
+        it('should add a tag', () => {
+          const tagsInstance = new BlipTags(tagList)
+          tagsInstance._addTag('Tag')
+
+          expect(tagsInstance.tags.filter(t => t.label === 'Tag').length > 0).toBeTruthy()
+        })
+      })
+
+      describe('Remove', () => {
+        it('should remove a tag', () => {
+          const tagsInstance = new BlipTags(tagList)
+          const newTag = tagsInstance._addTag('Tag')
+
+          tagsInstance._removeTag({
+            $event: {
+              tag: {
+                label: newTag.label,
+                id: newTag.tagOptions.id,
+              },
+            },
+          })
+
+          expect(tagsInstance.tags.filter(t => t.label === 'Tag').length === 0).toBeTruthy()
+        })
+      })
+    })
+  })
+})

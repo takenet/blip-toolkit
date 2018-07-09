6c5c292153fad2578ac5ef1b3e8adf7205e746c3
samx <samuelmartins.sw@gmail.com>
2018-07-09 15:28:10 -0300

    Refactor tests to use new component structure

--- spec/tests/blipSelect.spec.js
+++ spec/tests/blipSelect.spec.js
@@ -1,49 +1,99 @@
 import { BlipSelect } from '../../src/components/blipSelect'
-import { BlipSelectBase } from '../../src/components/blipSelect/blipSelectBase'
-import { BlipSelectAdd } from '../../src/components/blipSelect/blipSelectAdd'
+const mockOptions = [
+  {
+    value: 'val1',
+    label: 'label 1',
+  },
+  {
+    value: 'val2',
+    label: 'label 2',
+  },
+]
 
 describe('BlipSelect', () => {
-  let selectElement
+  describe('Behaviors', () => {
+    describe('Static', () => {
+      it('should has a list of options', () => {
+        const component = new BlipSelect()
+        const renderedElement = component.render({
+          options: mockOptions,
+        })
 
-  beforeEach(() => {
-    const select = document.createElement('select')
-    select.id = 'jasmine-select'
-    document.body.appendChild(select)
-    select.innerHTML = '<option value="opt1">Option 1</option>'
-    selectElement = document.getElementById('jasmine-select')
-  })
-
-  describe('Instance', () => {
-    it('should return a BlipSelectAdd instance', () => {
-      const component = new BlipSelect(selectElement, {
-        mode: 'autocomplete',
-        canAddOption: {
-          text: 'Add option',
-        },
+        expect(renderedElement.querySelectorAll('li').length).toEqual(2)
       })
 
-      expect(component instanceof BlipSelectAdd).toBeTruthy()
-    })
+      it('should disable component', () => {
+        const component = new BlipSelect()
+        document.body.appendChild(component.render({
+          options: mockOptions,
+        }))
 
-    it('should return a BlipSelectBase instance', () => {
-      const component = new BlipSelect(selectElement)
-      expect(component instanceof BlipSelectBase).toBeTruthy()
-    })
+        component.isDisabled = true
 
-    it('should focus', () => {
-      const component = new BlipSelect(selectElement, {
-        mode: 'autocomplete',
-        onFocus: () => 'focused',
+        expect(component.input.disabled).toBeTruthy()
       })
 
-      component.selectOptions = {
-        value: 'val1',
-        label: 'label 1',
-      }
+      it('should enable component', () => {
+        const component = new BlipSelect()
+        document.body.appendChild(component.render({
+          options: mockOptions,
+        }))
 
-      spyOn(component.configOptions, 'onFocus')
-      component._onSelectFocus()
-      expect(component.configOptions.onFocus).toHaveBeenCalled()
+        component.isDisabled = false
+
+        expect(component.input.disabled).toBeFalsy()
+      })
+
+      describe('Instance methods', () => {
+        it('should set a value', () => {
+          const component = new BlipSelect()
+          document.body.appendChild(component.render({
+            options: mockOptions,
+            inputValue: 'val1',
+          }))
+
+          expect(component.input.value).toEqual('val1')
+        })
+
+        it('should modify default search', () => {
+          const component = new BlipSelect({
+            mode: 'autocomplete',
+            customSearch: ({ $event }) => {
+              const { query, items } = $event
+              return items.filter(i => i.label.endsWith(query))
+            },
+          })
+
+          document.body.appendChild(component.render({
+            options: mockOptions,
+          }))
+
+          component.setValue({ label: '1' })
+          component.input.dispatchEvent(new Event('input'))
+
+          expect(component.optionsList.props.options.length).toEqual(1)
+          expect(component.optionsList.props.options[0].label).toEqual('label 1')
+        })
+      })
+    })
+
+    describe('Creatable', () => {
+      it('should add new option', () => {
+        const component = new BlipSelect({
+          mode: 'autocomplete',
+          canAddOption: {
+            text: 'Add option',
+          },
+        })
+
+        document.body.appendChild(component.render({
+          options: mockOptions,
+        }))
+
+        component.optionsList.addOption('label 3')
+
+        expect(component.optionsList.props.options.some(o => o.label === 'label 3')).toBeTruthy()
+      })
     })
   })
 })

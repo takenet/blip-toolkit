454bd26e7f75576c204ec3f27b91492c9248c9e2
Albert Moreira <albertm@take.net>
2019-03-26 13:38:34 -0300

    feat(BlipTabs): Gradient border and IE support

--- src/components/blipTabs/index.js
+++ src/components/blipTabs/index.js
@@ -5,25 +5,27 @@ export const BlipTabs = (elementId) => {
   let tabContentDivs = tabsContainer.querySelectorAll('.tab-content')
   tabLinks[0].parentElement.classList.add('active')
   tabContentDivs[0].classList.add('visible')
-  tabLinks.forEach(tablink => tablink.addEventListener('click', event => showTab(event, tabLinks, tabContentDivs)))
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener('click', event => showTab(event, tabLinks, tabContentDivs))
+  }
 }
 
 const showTab = (event, tabLinks, tabContentDivs) => {
   event.preventDefault()
   let link = event.target
   let reference = link.dataset.ref
-  tabLinks.forEach(link => {
-    if (link.dataset.ref === reference) {
-      link.parentElement.classList.add('active')
+  for (let i = 0; i < tabLinks.length; i++) {
+    if (tabLinks[i].dataset.ref === reference) {
+      tabLinks[i].parentElement.classList.add('active')
     } else {
-      link.parentElement.classList.remove('active')
+      tabLinks[i].parentElement.classList.remove('active')
     }
-  })
-  tabContentDivs.forEach(content => {
-    if (content.dataset.ref === reference) {
-      content.classList.add('visible')
+  }
+  for (let i = 0; i < tabContentDivs.length; i++) {
+    if (tabContentDivs[i].dataset.ref === reference) {
+      tabContentDivs[i].classList.add('visible')
     } else {
-      content.classList.remove('visible')
+      tabContentDivs[i].classList.remove('visible')
     }
-  })
+  }
 }

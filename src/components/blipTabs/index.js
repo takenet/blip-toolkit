be98d9b188b070d8edc2bd9779d392bee993895e
samx <samuelmartins.sw@gmail.com>
2019-11-13 15:08:39 -0300

    feat(components): add possibility to set active tab at start of component
    feature/default-tab-bliptabs

--- src/components/blipTabs/index.js
+++ src/components/blipTabs/index.js
@@ -1,24 +1,45 @@
 export class BlipTabs {
-  constructor(elementId) {
+  constructor(elementId, options = {}) {
     this.elementId = elementId
+    this.options = options
+
     this.init()
   }
+
   init() {
+    const activeTab = this.options.activeTab
     const tabsContainer = document.getElementById(this.elementId)
-    tabsContainer.classList.add('bp-tabs-container')
     const tabLinks = tabsContainer.querySelectorAll('.bp-tab-nav a')
     const tabContentDivs = tabsContainer.querySelectorAll('.bp-tab-content')
-    tabLinks[0].parentElement.classList.add('bp-tab-active')
-    tabContentDivs[0].classList.add('bp-tab-content-visible')
+    tabsContainer.classList.add('bp-tabs-container')
+
+    let activeTabLink
+    let activeTabContainer
+
+    if (!activeTab) {
+      activeTabLink = tabLinks[0].parentElement
+      activeTabContainer = tabContentDivs[0]
+    } else {
+      const tabLinkSelector = `.bp-tab-nav a[data-ref="${activeTab}"]`
+      activeTabLink = (tabsContainer.querySelector(tabLinkSelector) &&
+        tabsContainer.querySelector(tabLinkSelector).parentElement) || tabLinks[0].parentElement
+
+      activeTabContainer = tabsContainer.querySelector(`.bp-tab-content[data-ref="${activeTab}"]`) || tabContentDivs[0]
+    }
+
+    activeTabLink && activeTabLink.classList.add('bp-tab-active')
+    activeTabContainer && activeTabContainer.classList.add('bp-tab-content-visible')
+
     for (let i = 0; i < tabLinks.length; i++) {
       tabLinks[i].addEventListener('click', event => this.showTab(event, tabLinks, tabContentDivs))
     }
   }
 
-  showTab = (event, tabLinks, tabContentDivs) => {
+  showTab(event, tabLinks, tabContentDivs) {
     event.preventDefault()
     const link = event.target
     const reference = link.dataset.ref
+
     for (let i = 0; i < tabLinks.length; i++) {
       if (tabLinks[i].dataset.ref === reference) {
         tabLinks[i].parentElement.classList.add('bp-tab-active')
@@ -26,6 +47,7 @@ export class BlipTabs {
         tabLinks[i].parentElement.classList.remove('bp-tab-active')
       }
     }
+
     for (let i = 0; i < tabContentDivs.length; i++) {
       if (tabContentDivs[i].dataset.ref === reference) {
         tabContentDivs[i].classList.add('bp-tab-content-visible')

dd9ec9754787954cd0cf31d2288e242e77eb0236
Nícolas Arruda Maduro <nicolasm@take.net>
2020-02-12 14:25:17 -0300

    fix(components): improving blip loading props params
    feature/151275

--- sandbox/blipLoading.js
+++ sandbox/blipLoading.js
@@ -1,7 +1,11 @@
-import { BlipLoading } from '../src/components/blipLoading'
+import {
+  BlipLoading,
+  LoadingSizes,
+  LoadingTypes,
+} from '../src/components/blipLoading'
 
-const blipLoadingComponentDotted = BlipLoading()
+const blipLoadingComponentDotted = BlipLoading({ size: LoadingSizes.Small })
 document.getElementById('dotted-loading').appendChild(blipLoadingComponentDotted)
 
-const blipLoadingComponentSolid = BlipLoading({type: 'solid'})
+const blipLoadingComponentSolid = BlipLoading({ type: LoadingTypes.Solid })
 document.getElementById('solid-loading').appendChild(blipLoadingComponentSolid)
